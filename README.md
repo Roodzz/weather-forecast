7d602db339cc1d3fe956cf5e9126f247d41836a8
Roodzz <guilhermerandrade95@gmail.com>
2024-09-23 22:22:26 -0300

    adding python file and partial README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# weather-forecast
-use of web scraping for weather forecasting based on computer address
+# Projeto de Web Scraping e Consulta de Clima (Weather Forecast)
+
+Este projeto demonstra o uso de algumas bibliotecas poderosas de Python para obter informações de clima e localização. Ele utiliza as bibliotecas `requests`, `BeautifulSoup`, e `Selenium` para realizar web scraping e interagir com websites, além de consultar a localização do usuário usando a API do `ipinfo.io`.
+
+## Funcionalidades
+
+- Consulta a localização do usuário (cidade, região, país) utilizando a API do `ipinfo.io`.
+- Busca a previsão do tempo atual para a cidade do usuário usando o `Google` ou, caso necessário, utilizando o `Selenium` para acessar o site `tempo.com`.
+- Remoção de acentos em strings para formatação adequada de URLs.
+
+## Tecnologias Utilizadas
+
+- **requests**: Para realizar requisições HTTP e obter a localização atual do usuário.
+- **BeautifulSoup**: Para realizar web scraping no Google e buscar dados de clima.
+- **Selenium**: Como alternativa para obter dados de clima diretamente do site `tempo.com` caso o web scraping falhe.
+- **WebDriver Manager**: Para gerenciar o driver do Chrome automaticamente no Selenium.
+- **Unicodedata**: Para remover acentos de strings e formatar textos.
+
+## Como Usar
+
+### Pré-requisitos
+
+- Python 3.x
+- Instalar as dependências do projeto:
+
+```bash
+pip install requests beautifulsoup4 selenium webdriver_manager
+```
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+import unicodedata
 
 response = requests.get('https://ipinfo.io')
 data = response.json()
@@ -12,6 +19,13 @@ print(f"Essa é sua localização!")
 print(f"Cidade: {city}")
 print(f"Região: {region}")
 print(f"País: {country}")
+
+def remover_acentos(texto):
+    texto = texto.replace(" ", "-").lower()
+    texto_normalizado = unicodedata.normalize('NFKD', texto)
+    return ''.join(c for c in texto_normalizado if not unicodedata.combining(c))
+
+city_formated = remover_acentos(city)
 
 def get_weather(city):
     url = f"https://www.google.com/search?q=clima+{city.replace(' ', '+')}"
@@ -27,10 +41,10 @@ def get_weather(city):
         temp = soup.find('span', attrs={'id': 'wob_tm'}).text
         condition = soup.find('span', attrs={'id': 'wob_dc'}).text
         # location = soup.find('div', attrs={'id': 'wob_loc'}).text
-        time = soup.find('div', attrs={'id': 'wob_dts'}).text
+        horario = soup.find('div', attrs={'id': 'wob_dts'}).text
         
         print(f"Localização: {city}")
-        print(f"Horário: {time}")
+        print(f"Horário: {horario}")
         print(f"Temperatura Atual: {temp}°C")
         print(f"Condição: {condition}")
 
@@ -48,7 +62,40 @@ def get_weather(city):
             
 
     except AttributeError:
-        print("Não foi possível encontrar informações climáticas para a cidade especificada.")
+        print("Não foi possível encontrar informações climáticas para a cidade especificada utilizando o bs4. Vamos utilizar o Selenium!")
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-extensions")
+
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        print(f"Acessando o site: Tempo.com")
+        driver.get(f"https://www.tempo.com/{city_formated}.htm")
+
+        time.sleep(5)
+
+        # print(f"""{driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[1]/span/span[1]').text} - {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[1]/span/span[2]').text}
+        # Máxima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[1]/span/span[4]/span[1]').text}
+        # Minima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[1]/span/span[4]/span[3]').text}
+        # """)
+        print(f"""{driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[2]/span/span[1]').text} - {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[2]/span/span[2]').text}
+        Máxima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[2]/span/span[4]/span[1]').text}
+        Minima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[2]/span/span[4]/span[3]').text}
+        """)
+        print(f"""{driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[3]/span/span[1]').text} - {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[3]/span/span[2]').text}
+        Máxima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[3]/span/span[4]/span[1]').text}
+        Minima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[3]/span/span[4]/span[3]').text}
+        """)
+        print(f"""{driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[4]/span/span[1]').text} - {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[4]/span/span[2]').text}
+        Máxima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[4]/span/span[4]/span[1]').text}
+        Minima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[4]/span/span[4]/span[3]').text}
+        """)
+        print(f"""{driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[5]/span/span[1]').text} - {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[5]/span/span[2]').text}
+        Máxima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[5]/span/span[4]/span[1]').text}
+        Minima: {driver.find_element(By.XPATH, '/html/body/main/div[2]/section/section[2]/div/ul/li[5]/span/span[4]/span[3]').text}
+        """)
+
+        driver.quit()
 
 # Exemplo de uso
 # city = input("Digite sua cidade: ")
